52fbf4a8a020170436530916fae4d0f222325669
dankkun <kwn1209@naver.com>
2024-03-22 17:23:30 +0900

    BoardUser 목록, 사용자 추가 기능

--- src/main/java/com/board/user/domain/UserVo.java
+++ src/main/java/com/board/user/domain/UserVo.java
@@ -2,15 +2,17 @@ package com.board.user.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// @Data: @Getter, @Setter, @ToString, @NoargsConstructor, @EqualsAndHashCode, @RequiredArgsConstructor
+// @Data: @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserVo {
 	private String userid;
 	private String passwd;
 	private String username;
 	private String email;
-	private int    upoint;
+	private String upoint;
 	private String indate;
 }
